7a19d00bf47b70a7cee5de603e08c1bceb699c6a
Dorian De Simone <dorian@absolutelabs.io>
2024-05-14 14:54:43 -0400

    test

---
+++ app/components/Client.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React, { useEffect } from 'react'
+
+const Client = () => {
+  useEffect(() => {
+    console.log("page mounted")
+  }, [])
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Client
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-"use client"
-
-import { useEffect } from 'react'
 import { getFrameMetadata } from 'frog/next'
 import type { Metadata } from 'next'
 import Image from 'next/image'
 
 import styles from './page.module.css'
+import Client from './components/client'
 
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
@@ -17,12 +15,9 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default function Home() {
-  useEffect(() => {
-    console.log("page mounted")
-  }, [])
-
   return (
     <main className={styles.main}>
+      <Client />
       <div className={styles.description}>
         <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
           <p>
