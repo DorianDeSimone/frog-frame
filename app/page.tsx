4eb6ec805288a815189c5828e07ee8d13b662098
Dorian De Simone <dorian@absolutelabs.io>
2024-05-14 15:01:41 -0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import { getFrameMetadata } from 'frog/next'
 import type { Metadata } from 'next'
 import Image from 'next/image'
-
 import styles from './page.module.css'
-import ClientComp from './components/ClientComp'
 
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
@@ -17,7 +15,6 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function Home() {
   return (
     <main className={styles.main}>
-      <ClientComp />
       <div className={styles.description}>
         <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
           <p>
