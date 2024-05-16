01d9b34a261ed45edb83446d60416ca5b144b537
Dorian De Simone <dorian@absolutelabs.io>
2024-05-16 12:03:31 -0400

    constants

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,3 +1,11 @@
 export const BLUE = "#109cf1"
-
 export const RED = "#f7685b"
+export const FETCHING_FAILURE = "Failed to fetch form"
+export const INVALID_FORM = "Invalid form"
+export const EMAIL_PLACEHOLDER = "Enter your email..."
+export const BUTTON_SUBMIT = "Subscribe"
+export const BUTTON_RESET = "Retry"
+export const NOT_VERIFIED = "You are not verified"
+export const INVALID_EMAIL = "Invalid email"
+export const SUBMITTED = "Email sent"
+export const BUTTON_BACK = "Back home"
