8e5cc7ce6e0da9a3a00dba670069fabcbae5c7db
Dorian De Simone <dorian@absolutelabs.io>
2024-05-28 12:14:12 -0400

    url updated

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7gdnB/AbQuL7X4aBV7AWcjl88hnWMuiUZ++em1voisc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QLaLWytN0ZK9Al40yDYFQVuvEcwdW5A2e2pwlj05A5s=\"\n}"
